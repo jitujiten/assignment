29b3c29a6bba58f4b7868501bfdcd6342c93fda9
jitujiten <jsahu5425@gmail.com>
2023-09-14 11:53:42 +0530

    "add aditional content"

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Three.js React App</h1>
+        <h1>Three.js React App is live</h1>
         <ThreeScene />
       </header>
     </div>
